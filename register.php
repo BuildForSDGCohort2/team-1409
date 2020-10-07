45c70a80fbf4907a130c74f5e8eee2ff0726907a
seffu <you@example.com>
2020-10-07 20:59:01 +0300

    this has all ui working perfectly

--- register.php
+++ register.php
@@ -6,38 +6,41 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Safe Gas</title>
-    <link rel="stylesheet" href="contact.css">
+    <link rel="stylesheet" href="registration.css">
     <link rel="stylesheet" href="styles.css">
     <script src="bootstrap/js/jquery.min.js"></script>
     <script src="bootstrap/js/popper.min.js"></script>
     <script src="bootstrap/js/bootstrap.js"></script>
 </head>
-<body>
 <header>
     <h1>SAFE<span>GAS</span></h1>
     <nav>
         <ul>
             <li><a href="index.html"><b>Home</b></a></li>
-            <li><a href="login.html"><b>Register</b></a></li>
             <li><a href="contact.html"><b>Contact Us</b></a></li>
-            <li><a href="about.html"><b>About</b></a></li>
+            <li><a href="login.html"><b>Log In</b></a></li>
         </ul>
     </nav>
 </header>
-<form action="reg_con.php" method="POST">
-    <label>
-        <input type="text" name="x" placeholder="Name" class="form-group" >
-    </label><br>
-    <label>
-        <input type="email" name="y" placeholder="Email" class="form-group" >
-    </label><br>
-    <label>
-        <input type="password" name="z" placeholder="Password" >
-    </label><br>
-    <label>
-        <input type="password" name="b" placeholder="Confirm Password" >
-    </label><br>
-    <input type="submit" name="btnsubmit" value="Save" class="btn btn-success">
+<body>
+<div>
+    <h1 class="kichwa">Fill In Your Details</h1>
+</div>
+<div class="contact-title">
+    <h2>Click The Register Button Once You;re Done</h2>
+
+</div>
+<div class="registration-form">
+<form id="registration-form" class="registration-form" method="POST" action="reg_con.php">
+    <input type="text" class="reg-control" name="fname" placeholder="First Name" required><br>
+    <input type="text" class="reg-control" name="lname" placeholder="Last Name" required><br>
+    <input type="text" class="reg-control" name="cname" placeholder="Name of your company" required><br>
+    <input type="text" class="reg-control" name="email" placeholder="Company email" required><br>
+    <input type="password" class="reg-control" name="passworda" placeholder="Enter Password" required><br>
+    <input type="password" class="reg-control" name="passwordb" placeholder="Confirm Password" required><br>
+    <input type="submit" class="reg-control submit" value="REGISTER">
+    <!--        <button type="button" class="btn btn-warning">SEND MESSAGE</button>-->
+    </div>
 </form>
 </body>
 </html>
